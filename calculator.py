1b5165fd44648e1e0a423eb3a4b2f8b2318b14b8
Ianmendonca <fransonmendonca1996@gmail.com>
2025-03-19 09:12:19 +0100

    added value to subtract

--- calculator.py
+++ calculator.py
@@ -1,10 +1,10 @@
 # calculator.py
 class Calculator:
-    def add(self, x, y):
-        return x + y
+    def add(self, x, y,z):
+        return x + y + z
     
-    def subtract(self, x, y):
-        return x - y
+    def subtract(self, x, y, z):
+        return x - y -z
     
     def multiply(self, x, y):
         return x * y
@@ -13,3 +13,6 @@ class Calculator:
         if y == 0:
             raise ValueError("Cannot divide by zero")
         return x / y
+    
+calc = Calculator()
+print(calc.add(1,2,3))
